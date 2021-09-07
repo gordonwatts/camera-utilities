9558bf9b995abc68573ed383e0f2aaeccdb61466
Gordon Watts <gwatts@uw.edu>
2021-09-07 10:58:27 +0200

    Add ability to redirect the source
    From the default D:\DCIM directory
    
    Uses the -Source parameter.

--- Move-CameraImages.ps1
+++ Move-CameraImages.ps1
@@ -1,10 +1,20 @@
 # Move camera images into default final directories
 Function Move-CameraImages {
     [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false,
+            HelpMessage="Directory to get files from.")]
+            [System.IO.DirectoryInfo]$Source
+    )
 
     $pictures_folder = [environment]::getfolderpath("mypictures") 
 
-    $picture_move_info = Get-ChildItem d:\DCIM -Recurse -File `
+    $source_folder = [System.IO.DirectoryInfo] "D:\DCIM"
+    if ($Source) {
+        $source_folder = $Source
+    }
+
+    $picture_move_info = Get-ChildItem $source_folder -Recurse -File `
                          | Get-DesiredPicturePath -destination $pictures_folder `
                          | ForEach-Object {
                              if (-not (Test-Path $_.DestinationPath.Directory)) {
